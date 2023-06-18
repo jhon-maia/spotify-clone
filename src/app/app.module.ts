15ae52c487b2ab64dfc6e0b457c2a595b2e20163
jhon-maia <joaomaia171820@gmail.com>
2023-06-18 12:38:29 -0300

    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ScreenComponent } from './components/screen/screen.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContainerComponent } from './components/container/container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ScreenComponent,
+    SideBarComponent,
+    FooterComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule,
